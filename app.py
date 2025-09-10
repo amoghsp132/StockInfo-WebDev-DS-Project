ef1863df69c647c36eb9ad7fdddf55add454dd41
amoghsp132 <amoghpatil132@gmail.com>
2025-09-10 21:56:44 +0530

    added requirements and runtime txt

--- app.py
+++ app.py
@@ -252,4 +252,6 @@ def donate():
     return render_template('donate.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
